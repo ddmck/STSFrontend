a9b24cdefa17df08ade57a20b6718246963499ca
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-03 16:38:15 +0000

    pull in extra images if there are any

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('App', ['infinite-scroll', 'ngSanitize', 'ui.router', 'ng-token-auth', 'ipCookie', 'ngStorage', 'angularPayments']);
-var backendUrl = "http://localhost:3000/";
+var backendUrl = "https://www.shopshopgo.com/";
 Stripe.setPublishableKey('pk_test_mfQJDA4oT57DLFi7l0HYu782');
 
 app.config(function($stateProvider, $urlRouterProvider, $authProvider) {
