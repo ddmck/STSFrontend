84e02772b3e59e82794c0d190187e69def285bc3
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-12 22:16:01 +0000

    add live key

--- src/js/app/init-prod.js
+++ src/js/app/init-prod.js
@@ -1,3 +1,3 @@
 var app = angular.module('App', ['infinite-scroll', 'ngSanitize', 'ui.router', 'ng-token-auth', 'ipCookie', 'ngStorage', 'angularPayments']);
 var backendUrl = "https://www.shopshopgo.com/";
-Stripe.setPublishableKey('pk_test_mfQJDA4oT57DLFi7l0HYu782');
+Stripe.setPublishableKey('pk_live_j9uqoLXHPhq1clGi5jDnIWpy');
