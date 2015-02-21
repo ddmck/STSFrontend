f8681821060749292d92d756613599cbff26163a
Donald McKendrick <ddmckendrick@gmail.com>
2015-02-21 01:55:53 +0000

    call Meta function correctly

--- src/js/app/config.js
+++ src/js/app/config.js
@@ -19,7 +19,7 @@ app.config(function($stateProvider, $urlRouterProvider, $authProvider, $location
         } else {
           $scope.msg = "All The Best Stores - One Basket";
         };
-        Meta.set.title = "Welcome";
+        Meta.set("title", "Welcome");
         $scope.wishlist = $localStorage.wishlistItems;
         var animationDelay = 2500;
  
