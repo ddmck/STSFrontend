6a086d308e201efd79d34d45ad6c354a802ee14b
Donald McKendrick <ddmckendrick@gmail.com>
2015-03-24 15:59:06 +0000

    Add trends to homepage

--- src/js/app/init-dev.js
+++ src/js/app/init-dev.js
@@ -1,4 +1,4 @@
 var app = angular.module('App', ['infinite-scroll', 'ngSanitize', 'btford.markdown', 'ui.router', 'ng-token-auth', 'ipCookie', 'ngStorage', 'angularPayments', 'btford.modal', 'akoenig.deckgrid']);
-var backendUrl = "http://localhost:3000/";
+var backendUrl = "https://www.shopshopgo.com/";
 var assetsUrl = 'http://localhost:9000/';
 Stripe.setPublishableKey('pk_test_mfQJDA4oT57DLFi7l0HYu782');
