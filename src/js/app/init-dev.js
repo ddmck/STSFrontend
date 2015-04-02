3675b7638a89d509c1049a59cbf03ef8ddc0b965
Donald McKendrick <ddmckendrick@gmail.com>
2015-04-02 14:24:44 +0100

    Tidy fonts

--- src/js/app/init-dev.js
+++ src/js/app/init-dev.js
@@ -1,3 +1,3 @@
 var app = angular.module('App', ['infinite-scroll', 'ngSanitize', 'btford.markdown', 'ui.router', 'ng-token-auth', 'ipCookie', 'ngStorage', 'angularPayments', 'btford.modal', 'akoenig.deckgrid']);
-var backendUrl = "http://localhost:3000/";
+var backendUrl = "https://www.shopshopgo.com/";
 var assetsUrl = 'http://localhost:9999/';
